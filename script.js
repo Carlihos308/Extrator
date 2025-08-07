fb6533e02dafb245151fcdf2a1d17ce87d1b3c37
Carlihos308 <carlos.fanti@escola.pr.gov.br>
2025-08-07 11:42:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const botaoMostraPalavras= document.querySelector('#botao-palavrachave');
+botaoMostraPalavras.addEventListener('click',mostraPalavraChave);
+function mostraPalavraChave(){
+    alert('Fui clicado')
+}
